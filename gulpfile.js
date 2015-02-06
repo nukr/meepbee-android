d3f8125a7cdd94a81198c903ebf8465c74885f28
Nukr <nukrs.w@gmail.com>
2015-02-06 16:18:21 +0800

    add gulp task android

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var cp = require('child_process');
 require('require-dir')('./gulp-tasks');
 
-gulp.task('default', ['copy:assets', 'build:html', 'build:react', 'watch']);
+gulp.task('default', ['copy:assets', 'build', 'watch']);
+
+gulp.task('android', ['copy:assets', 'build', ''])
 
 gulp.task('open:browser', function () {
   cp.exec('open http://localhost:3000/client/build')
-})
+});
 
-gulp.task('dev', ['build:html', 'server:spawn', 'open:browser'])
+gulp.task('dev', ['build:html', 'server:spawn', 'open:browser']);
 
