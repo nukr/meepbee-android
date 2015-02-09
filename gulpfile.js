4c676329839c482978a8e8b9273cfda4f3585c57
Nukr <nukrs.w@gmail.com>
2015-02-09 14:29:37 +0800

    微調gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var cp = require('child_process');
 require('require-dir')('./gulp-tasks');
 
-gulp.task('default', ['copy:assets', 'build', 'watch']);
+gulp.task('default', ['build', 'watch'], function () {
+  gulp.start('server:spawn');
+});
 
-gulp.task('android', ['copy:assets', 'build', ''])
+gulp.task('android', ['copy:assets', 'build']);
 
 gulp.task('open:browser', function () {
   cp.exec('open http://localhost:3000/client/build')
